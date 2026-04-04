7615e82c3edf3eed80cb3c5e5dcd7e4361c5d292
UmmIt <git@ummit.dev>
2026-04-04 18:25:40 +0800

    fix: add paper OG image generation for individual posts

---
+++ src/pages/og/paper/[...slug].png.ts
@@ -0,0 +1,17 @@
+import type { APIRoute, GetStaticPaths } from 'astro'
+import { getCollection } from 'astro:content'
+import { generateOgImage, ogResponse } from '@/utils/og-image'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paperPosts = await getCollection('paper')
+  return paperPosts.map(post => ({
+    params: { slug: post.slug },
+    props: { title: post.data.title },
+  }))
+}
+
+export const GET: APIRoute = async ({ props }) => {
+  const { title } = props as { title: string }
+  const png = await generateOgImage('Paper', title)
+  return ogResponse(png)
+}
